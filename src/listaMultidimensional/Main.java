1047c89dde06eb2425806497e190204980724527
gugacaste <103458325+gugacaste@users.noreply.github.com>
2023-04-23 18:41:32 -0300

    Add files via upload

--- src/listaMultidimensional/Main.java
+++ src/listaMultidimensional/Main.java
@@ -1,25 +1,57 @@
 package listaMultidimensional;
 
-import java.util.Arrays;
-
 public class Main {
 
 	public static void main(String[] args) {
 
 		
 		
-		int[][] matriz = {{1}, {4, 5}, {6, 7, 8, 9}};
-		int[][] novaMatriz = removerPrimeira(matriz);
-		System.out.println(Arrays.deepToString(novaMatriz));
+		int[][] matriz = {{1,2,3}, {4, 5}, {6, 7, 8, 9}};
+		
+//		Para remover primeiro elemento :
+		
+//		int[][] novaMatriz = removerPrimeira(matriz);
+//		imprimirMatriz(novaMatriz);
+//		_________________________________________________________
+		
+//		Para inverter a Matriz :
+//		int[][] Matriz4 = inverterMatriz(matriz);
+//		imprimirMatriz(Matriz4);
+//		_________________________________________________________
+		
+//		Para adicionar um elemento no final da Matriz:
+//		int [][] matriz5 = adicionarElementoFinal(matriz, 10);
+//		imprimirMatriz(matriz5);
+//		_________________________________________________________
 		 int[] lista = {10, 11, 12};
-//		 int[][] Matriz2 = adicionarListaFinal(matriz, lista);
-//		 System.out.println(Arrays.deepToString(Matriz2));
-		 // Adicionar lista no final da matriz-----------
-//		 int[][] Matriz3 = adicionarListaFinal(Matriz2, lista);
-//		 System.out.println(Arrays.deepToString(Matriz3));
+		 int[][] Matriz2 = adicionarListaFinal(matriz, lista);
+		 imprimirMatriz(Matriz2);
+//		  Adicionar lista no final da matriz-----------
+		 int[][] Matriz3 = adicionarListaFinal(Matriz2, lista);
+		 imprimirMatriz(Matriz3);
 		 
 
 	}
+	public static void imprimirMatriz(int[][] matriz) {
+	    for (int i = 0; i < matriz.length; i++) {
+	        for (int j = 0; j < matriz[i].length; j++) {
+	            System.out.print(matriz[i][j] + " ");
+	        }
+	        System.out.println();
+	    }System.out.println();
+	}
+	
+	public static int[][] inverterMatriz(int[][] matriz) {
+	    int[][] novaMatriz = new int[matriz.length][];
+	    for (int i = 0; i < matriz.length; i++) {
+	        novaMatriz[i] = new int[matriz[i].length];
+	        for (int j = 0; j < matriz[i].length; j++) {
+	            novaMatriz[i][j] = matriz[i][matriz[i].length - 1 - j];
+	        }
+	    }
+	    return novaMatriz;
+	}
+	
 	private static int[][] adicionarListaFinal(int[][] matriz, int[] lista) {
 	    int linhas = matriz.length;
 	    int colunas = matriz[0].length;
@@ -59,6 +91,20 @@ public class Main {
 		        }
 		    }
 		    return novaMatriz;
+	}
+	public static int[][] adicionarElementoFinal(int[][] matriz, int elemento) {
+	    int linha = matriz.length - 1;
+	    int coluna = matriz[linha].length;
+	    int[][] novaMatriz = new int[linha + 1][coluna + 1];
+	    for (int i = 0; i < linha; i++) {
+	        novaMatriz[i] = matriz[i];
+	    }
+	    novaMatriz[linha] = new int[coluna + 1];
+	    for (int i = 0; i < coluna; i++) {
+	        novaMatriz[linha][i] = matriz[linha][i];
+	    }
+	    novaMatriz[linha][coluna] = elemento;
+	    return novaMatriz;
 	}
 
 
